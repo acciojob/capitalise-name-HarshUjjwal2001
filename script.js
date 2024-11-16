08afdbac87d984e38c82d6e20c01630885a1e796
HarshUjjwal2001 <harshujjwal2002@gmail.com>
2024-11-16 10:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+    if( str == null || str.length == 0)
+    {
+        return []
+    }
+
+    let ans = []
+    let i = 0
+
+    while(i < str.length)
+    {
+        let temp = str.slice(i,size + i)
+        ans.push(temp)
+        i += size
+    }
+
+    return ans
+
+  }
